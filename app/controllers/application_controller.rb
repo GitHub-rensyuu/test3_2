b03508873fe173ef1b570fc6d1e4c125a41af538
EC2 Default User <ec2-user@ip-172-31-33-222.ec2.internal>
2022-03-23 23:36:48 +0900

    課題3二回目

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+   protected
+  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end
+
+end
